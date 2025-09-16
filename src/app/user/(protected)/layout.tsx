fd885dd8f20367e10b225dcae7049c7a727cabd0
Devansh <devanshd78@gmail.com>
2025-09-16 17:51:18 +0530

    updates

--- src/app/user/(protected)/layout.tsx
+++ src/app/user/(protected)/layout.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
-export default function EmployeeLayout({
+export default function UserLayout({
   children,
 }: {
   children: React.ReactNode
